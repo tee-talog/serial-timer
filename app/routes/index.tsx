4bd87d268af23432045c344526eab312650e352f
talog <tee.talog@gmail.com>
2025-04-06 18:16:52 +0900

    HTML base

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,79 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+const IntervalList = () => {
+  return (
+    <div>
+      <span>1/2</span>
+      <ol>
+        <li>2:00</li>
+        <li>3:00</li>
+      </ol>
+    </div>
+  );
+};
+
+const TotalTimes = () => {
+  return (
+    <table>
+      <tr>
+        <td>合計経過時間</td>
+        <td>
+          <time dateTime="PT10M">10:00</time>
+        </td>
+      </tr>
+
+      <tr>
+        <td>合計残り時間</td>
+        <td>
+          <time dateTime="PT15M">15:00</time>
+        </td>
+      </tr>
+    </table>
+  );
+};
+
+const IndividualTimes = () => {
+  return (
+    <table>
+      <tr>
+        <td>現在のタイマーの経過時間</td>
+        <td>
+          <time dateTime="PT2M1S">2:01</time>
+        </td>
+      </tr>
+
+      <tr>
+        <td>現在のタイマーの残り時間</td>
+        <td>
+          <time dateTime="PT2M1S">2:01</time>
+        </td>
+      </tr>
+    </table>
+  );
+};
+
+const Actions = () => {
+  return (
+    <div>
+      <button type="button">start</button>
+      <button type="button">pause</button>
+      <button type="button">stop</button>
+    </div>
+  );
+};
+
 const RouteComponent = () => {
   return (
     <div>
-      <header>header</header>
-      <main>main</main>
-      <footer>footer</footer>
+      <header>連タイマー</header>
+
+      <main>
+        <TotalTimes />
+        <IndividualTimes />
+        <Actions />
+
+        <IntervalList />
+      </main>
     </div>
   );
 };

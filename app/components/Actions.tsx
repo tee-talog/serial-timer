2c91daf2db07474ede9ff8875e98c1199f73e825
talog <tee.talog@gmail.com>
2025-06-01 21:02:04 +0900

    actions style

--- app/components/Actions.tsx
+++ app/components/Actions.tsx
@@ -1,13 +1,16 @@
 import type { FC } from "react";
 import { Button } from "./uis/Button";
+import clsx from "clsx";
 
 export const Actions: FC<{
   onStart: () => void;
   onPause: () => void;
   onStop: () => void;
 }> = ({ onStart, onPause, onStop }) => {
+  const divStyle = clsx("flex", "gap-x-4");
+
   return (
-    <div>
+    <div className={divStyle}>
       <Button type="button" onClick={onStart}>
         start
       </Button>

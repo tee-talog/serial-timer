05bfffa894b863eba909238dbe3ad3ee518dd23c
talog <tee.talog@gmail.com>
2025-06-01 16:49:42 +0900

    input style

--- app/components/uis/Input.tsx
+++ app/components/uis/Input.tsx
@@ -1,5 +1,19 @@
+import clsx from "clsx";
 import type { ComponentProps, FC } from "react";
 
 type Props = ComponentProps<"input">;
 
-export const Input: FC<Props> = (props) => <input {...props} />;
+export const Input: FC<Props> = ({ className, ...props }) => {
+  const style = clsx(
+    className,
+    "size-[2em]",
+    "w-max",
+    "px-3",
+    "border-2",
+    "border-slate-500",
+    "rounded-[0.25em]",
+  );
+  const focusStyle = clsx("focus:bg-slate-100");
+
+  return <input {...props} className={clsx(style, focusStyle)} />;
+};

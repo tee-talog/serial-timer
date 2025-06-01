70e0c0db35465b5f4bee46bf7f6727de6eb1b44e
talog <tee.talog@gmail.com>
2025-06-01 15:33:41 +0900

    emotion

--- app/components/layouts/AppLayout.tsx
+++ app/components/layouts/AppLayout.tsx
@@ -1,18 +1,17 @@
 import type { ComponentProps, FC } from "react";
 import { clsx } from "clsx";
+// @emotion/react を使おうとすると css プロパティの追加がされなくてスタイルが適用されない
+import { css } from "@emotion/css";
 
 type Props = ComponentProps<"div">;
 
 export const AppLayout: FC<Props> = ({ className, ...props }) => {
-  const styleGrid = [
-    "grid",
-    "grid-rows-[60px_1fr]",
-    "gap-y-4",
-    "grid-cols-[1fr_minmax(auto,800px)_1fr]",
-    "gap-x-2",
-  ];
+  const styleGrid = ["grid", "gap-y-4", "gap-x-2"];
+  const style = css`
+    grid-template-rows: 60px 1fr;
+    grid-template-columns: 1fr minmax(auto, 800px) 1fr;
+  `;
+  const _className = clsx(className, ...styleGrid, style);
 
-  const cn = clsx(className, ...styleGrid);
-
-  return <div className={cn} {...props} />;
+  return <div {...props} className={_className} />;
 };

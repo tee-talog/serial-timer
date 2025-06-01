d2197a882ea7e434ce688eba58c835bdd6079ec2
talog <tee.talog@gmail.com>
2025-06-01 21:07:17 +0900

    form style

--- app/components/AddTime.tsx
+++ app/components/AddTime.tsx
@@ -1,14 +1,18 @@
 import { type FC, useState } from "react";
 import { Button } from "./uis/Button";
 import { Input } from "./uis/Input";
+import clsx from "clsx";
 
 export const AddTime: FC<{
   addTimer: (num: number) => void;
 }> = ({ addTimer }) => {
+  const formStyle = clsx("flex", "gap-x-2");
+
   const [num, setNum] = useState(0);
 
   return (
     <form
+      className={formStyle}
       onSubmit={(e) => {
         e.preventDefault();
         addTimer(num);

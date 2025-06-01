b63e79cdcb327929228117139a36b7240b35e9aa
talog <tee.talog@gmail.com>
2025-06-01 20:08:27 +0900

    table style

--- app/components/uis/Td.tsx
+++ app/components/uis/Td.tsx
@@ -1,5 +1,10 @@
+import clsx from "clsx";
 import type { ComponentProps, FC } from "react";
 
 type Props = ComponentProps<"td">;
 
-export const Td: FC<Props> = (props) => <td {...props} />;
+export const Td: FC<Props> = ({ className, ...props }) => {
+  const style = clsx(className, "px-2", "first:pl-0", "last:pr-0");
+
+  return <td {...props} className={style} />;
+};
--- app/components/uis/Tr.tsx
+++ app/components/uis/Tr.tsx
@@ -1,5 +1,10 @@
+import clsx from "clsx";
 import type { ComponentProps, FC } from "react";
 
 type Props = ComponentProps<"tr">;
 
-export const Tr: FC<Props> = (props) => <tr {...props} />;
+export const Tr: FC<Props> = ({ className, ...props }) => {
+  const style = clsx(className, "py-1");
+
+  return <tr {...props} className={style} />;
+};

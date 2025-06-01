4e6d14bffff9193891e5fc0f929e629fa6589638
talog <tee.talog@gmail.com>
2025-06-01 16:26:41 +0900

    button style

--- app/components/uis/Button.tsx
+++ app/components/uis/Button.tsx
@@ -1,5 +1,21 @@
+import clsx from "clsx";
 import type { ComponentProps, FC } from "react";
 
 type Props = ComponentProps<"button">;
 
-export const Button: FC<Props> = (props) => <button {...props} />;
+export const Button: FC<Props> = ({ className, ...props }) => {
+  const style = clsx(
+    className,
+    "size-[2em]",
+    "w-max",
+    "px-3",
+    "border-2",
+    "border-slate-500",
+    "rounded-[0.25em]",
+  );
+
+  const hoverStyle = clsx("hover:bg-slate-100");
+  const activeStyle = clsx("active:bg-slate-200");
+
+  return <button {...props} className={clsx(style, hoverStyle, activeStyle)} />;
+};

d4bb8c73fded427a089cd1e816758bdbe1ebfafa
talog <tee.talog@gmail.com>
2025-06-01 18:29:10 +0900

    page layout style

--- app/components/layouts/PageLayout.tsx
+++ app/components/layouts/PageLayout.tsx
@@ -4,7 +4,15 @@ import type { ComponentProps, FC } from "react";
 type Props = ComponentProps<"main">;
 
 export const PageLayout: FC<Props> = ({ className, ...props }) => {
-  const cn = clsx(className, "row-2", "col-2");
+  const cn = clsx(
+    className,
+    "row-2",
+    "col-2",
+    "flex",
+    "flex-col",
+    "items-start",
+    "gap-y-4",
+  );
 
   return <main className={cn} {...props} />;
 };

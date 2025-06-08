5c0bc66e86416ad3b312a890994a2eb99fb5671a
talog <tee.talog@gmail.com>
2025-06-08 14:58:15 +0900

    style summary time

--- app/components/TotalTimes.tsx
+++ app/components/TotalTimes.tsx
@@ -3,6 +3,7 @@ import { Table } from "./uis/Table";
 import { Td } from "./uis/Td";
 import { Tr } from "./uis/Tr";
 import { TBody } from "./uis/TBody";
+import clsx from "clsx";
 
 // 合計時間
 export const TotalTimes: FC<{ timeSum: number; remainTime: number }> = ({
@@ -21,23 +22,52 @@ export const TotalTimes: FC<{ timeSum: number; remainTime: number }> = ({
   // TODO
   const elapsedTimeDuration = formattedElapsedTime;
 
-  return (
-    <Table>
-      <TBody>
-        <Tr>
-          <Td>合計経過時間</Td>
-          <Td>
-            <time dateTime={elapsedTimeDuration}>{formattedElapsedTime}</time>
-          </Td>
-        </Tr>
+  const styleSection = clsx("border", "border-slate-400", "rounded", "w-full");
 
-        <Tr>
-          <Td>合計残り時間</Td>
-          <Td>
-            <time dateTime={remainTimeDuration}>{formattedRemainTime}</time>
-          </Td>
-        </Tr>
-      </TBody>
-    </Table>
+  const styleTitle = clsx(
+    "text-2xl",
+    "font-bold",
+    "p-4",
+    "py-2",
+    "border-b",
+    "border-slate-400",
+  );
+
+  const styleTable = clsx("p-4", "pt-2");
+
+  const styleRemainTitle = clsx("text-4xl", "leading-[1.6]");
+  const styleRemainTime = clsx("text-4xl", "text-right");
+
+  const styleElapsedTitle = clsx("text-xl");
+  const styleElapsedTime = clsx("text-xl", "text-right");
+
+  return (
+    <section className={styleSection}>
+      <h2 className={styleTitle}>合計</h2>
+
+      <div className={styleTable}>
+        <Table>
+          <TBody>
+            <Tr>
+              <Td className={styleRemainTitle}>残り時間</Td>
+              <Td className={styleRemainTime}>
+                <time dateTime={remainTimeDuration}>{formattedRemainTime}</time>
+                秒
+              </Td>
+            </Tr>
+
+            <Tr>
+              <Td className={styleElapsedTitle}>経過時間</Td>
+              <Td className={styleElapsedTime}>
+                <time dateTime={elapsedTimeDuration}>
+                  {formattedElapsedTime}
+                </time>
+                秒
+              </Td>
+            </Tr>
+          </TBody>
+        </Table>
+      </div>
+    </section>
   );
 };
--- app/components/uis/Td.tsx
+++ app/components/uis/Td.tsx
@@ -4,7 +4,7 @@ import type { ComponentProps, FC } from "react";
 type Props = ComponentProps<"td">;
 
 export const Td: FC<Props> = ({ className, ...props }) => {
-  const style = clsx(className, "px-2", "first:pl-0", "last:pr-0");
+  const style = clsx(className, "px-[0.5em]", "first:pl-0", "last:pr-0");
 
   return <td {...props} className={style} />;
 };

a10cf048424b11a601f0b1412a3686208736d5e5
talog <tee.talog@gmail.com>
2025-09-28 14:51:49 +0900

    no plugin

--- app.config.ts
+++ app.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "@tanstack/react-start/config";
 import tsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
-import { cloudflare } from "@cloudflare/vite-plugin";
+// import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   vite: {
     plugins: [
-      cloudflare({ viteEnvironment: { name: "ssr" } }),
+      // cloudflare({ viteEnvironment: { name: "ssr" } }),
       tsConfigPaths({
         projects: ["./tsconfig.json"],
       }),
